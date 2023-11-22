8eaecee993fc2bd61bd2596a190959ed1bfae894
lxj <2256996378@qq.com>
2023-11-22 21:06:30 +0800

    小程序API的Promise化

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 // app.js
+
+//在小程序入口文件中(app.js)，只需要调用一次promisifyAll()方法
+//即可实现异步API的Promise化
+import {promisifyAll} from 'miniprogram-api-promise'
+
+const wxp = wx.p = {}
+
+//promisify all wx's api 
+promiseIfyAll(wx,wxp)
+
+
 App({
   onLaunch() {
     // 展示本地存储能力
